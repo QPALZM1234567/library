72c754545c41a9a1bd1d4dca295e87e478a4eab6
Sean <seanjung03@gmail.com>
2022-08-12 10:57:32 -0400

    Add initial add button functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const body = document.querySelector('body');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -38,6 +39,39 @@ function displayBooks() {
     }
     books.appendChild(button);
 }
+
+const add = document.querySelector('#add');
+add.addEventListener('click', () => {
+    let background = document.createElement('div');
+    background.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    background.style.position = "absolute";
+    background.style.top = "0";
+    background.style.left = "0";
+    background.style.width = "100%";
+    background.style.height = "100%";
+    background.style.display = "flex";
+    background.style.justifyContent = "center";
+    background.style.alignItems = "center";
+
+    let form = document.createElement('form');
+    form.setAttribute("action", "script.js");
+    form.setAttribute("method", "post");
+
+    let label = document.createElement('label');
+    label.setAttribute("for", "title");
+    label.textContent = "Title: ";
+    let input = document.createElement('input');
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "title");
+    input.setAttribute("name", "title");
+    form.appendChild(label);
+    form.appendChild(input);
+
+
+    background.appendChild(form);
+    body.appendChild(background);
+
+});
 
 addBookToLibrary('Biography', 'Sean Jung', 197, true);
 addBookToLibrary('Biography', 'Sean Jung', 197, true);
